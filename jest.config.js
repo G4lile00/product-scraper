91bf8e4d0219b8585953f95fb2cca4b452f27a03
Vitor Vicente <vitor.vicente2023@gmail.com>
2024-02-16 15:30:55 -0300

    FIX: remove unused jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testMatch: ['<rootDir>/test/unit/**/*.spec.ts'],
-	setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
-  setupFilesAfterEnv: ['<rootDir>/test/unit/mocker.ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/unit/**/*.spec.ts'],
+  setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@test/(.*)$': '<rootDir>/test/$1',
-    }
+  }
 };
